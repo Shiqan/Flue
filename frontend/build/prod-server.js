294fdc8ad07419eb356794cc695c88f8b9483d0c
ferron <ferron.saan@live.nl>
2017-12-03 22:25:38 +0100

    update package.json

--- frontend/build/prod-server.js
+++ frontend/build/prod-server.js
@@ -5,8 +5,8 @@ app = express()
 // handle fallback for HTML5 history API
 app.use(require('connect-history-api-fallback')())
 
-app.use(serveStatic(__dirname))
+app.use(serveStatic(__dirname)+"/frontend/dist")
 var port = process.env.PORT || 5000
 var server = app.listen(port)
-console.log('> Starting dev server...')
+console.log('> Starting server...')
 console.log('> Listening at ' + port + '\n')
