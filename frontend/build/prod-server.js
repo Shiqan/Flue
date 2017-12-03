96e90e0610eedb5eb67dd236df0cb115a05fd522
ferron <ferron.saan@live.nl>
2017-12-03 22:35:55 +0100

    update package.json

--- frontend/build/prod-server.js
+++ frontend/build/prod-server.js
@@ -1,8 +1,6 @@
-require('./check-versions')()
-
 var config = require('../config')
 if (!process.env.NODE_ENV) {
-  process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
+  process.env.NODE_ENV = JSON.parse(config.prod.env.NODE_ENV)
 }
 
 var opn = require('opn')
@@ -10,9 +8,7 @@ var path = require('path')
 var express = require('express')
 var webpack = require('webpack')
 var proxyMiddleware = require('http-proxy-middleware')
-var webpackConfig = process.env.NODE_ENV === 'testing'
-  ? require('./webpack.prod.conf')
-  : require('./webpack.dev.conf')
+var webpackConfig = require('./webpack.prod.conf')
 
 // default port where dev server listens for incoming traffic
 var port = process.env.PORT || config.dev.port
@@ -75,9 +71,6 @@ console.log('> Starting dev server...')
 devMiddleware.waitUntilValid(() => {
   console.log('> Listening at ' + uri + '\n')
   // when env is testing, don't need open it
-  if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
-    opn(uri)
-  }
   _resolve()
 })
 
