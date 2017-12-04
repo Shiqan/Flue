d0de709566f97907c296117900949aef6406c81f
ferron <ferron.saan@live.nl>
2017-12-04 22:11:28 +0100

    remove host from waitress

--- backend/app.py
+++ backend/app.py
@@ -6,8 +6,6 @@ from falcon_cors import CORS
 from vainglory import Players, Matches
 
 cors = CORS(allow_all_origins=True, allow_origins_list=['http://localhost.com:8081'])
-
-
 api = application = falcon.API(middleware=[cors.middleware])
 
 players = Players()
@@ -16,4 +14,4 @@ api.add_route('/api/player/{region}/{name}', players)
 api.add_route('/api/matches/{region}/{name}', matches)
 
 port = int(os.environ.get('PORT', 33507))
-serve(api, host='0.0.0.0', port=port)
+serve(api, port=port)
